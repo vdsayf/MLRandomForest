5df4609e95325566e8479219cc513d28b0a72990
Victor Dai <vdsayf@gmail.com>
2022-11-23 05:49:42 -0800

    categorical done

--- categorical.py
+++ categorical.py
@@ -1,21 +1,94 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
 
 # Read the data
-X = pd.read_csv('../input/train.csv', index_col='Id') 
-X_test = pd.read_csv('../input/test.csv', index_col='Id')
+X = pd.read_csv('category5/train.csv', index_col='Id') 
+X_test = pd.read_csv('category5/test.csv', index_col='Id')
 
 # Remove rows with missing target, separate target from predictors
 X.dropna(axis=0, subset=['SalePrice'], inplace=True)
 y = X.SalePrice
 X.drop(['SalePrice'], axis=1, inplace=True)
 
-# To keep things simple, we'll drop columns with missing values
+#drop columns with missing values
 cols_with_missing = [col for col in X.columns if X[col].isnull().any()] 
 X.drop(cols_with_missing, axis=1, inplace=True)
 X_test.drop(cols_with_missing, axis=1, inplace=True)
 
 # Break off validation set from training data
-X_train, X_valid, y_train, y_valid = train_test_split(X, y,
-                                                      train_size=0.8, test_size=0.2,
-                                                      random_state=0)
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,random_state=0)
+
+def score_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=100, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
+
+#dropping categorical columns
+s = (X_train.dtypes == 'object')
+object_cols = list(s[s].index)
+
+drop_X_train = X_train.select_dtypes(exclude=['object'])
+drop_X_valid = X_valid.select_dtypes(exclude=['object'])
+
+#testing MAE
+print("MAE from Approach 1 (Drop categorical variables):")
+print(score_dataset(drop_X_train, drop_X_valid, y_train, y_valid))
+
+#Step 2 Ordinal
+#categorical column names
+object_cols = [col for col in X_train.columns if X_train[col].dtype == "object"]
+#columns that can be safely ordinal encoded
+good_label_cols = [col for col in object_cols if set(X_valid[col]).issubset(set(X_train[col]))]
+#columns we drop (bad labels)
+bad_label_cols = list(set(object_cols)-set(good_label_cols))
+
+#ordinally encode data 
+# Drop categorical columns that will not be encoded
+label_X_train = X_train.drop(bad_label_cols, axis=1)
+label_X_valid = X_valid.drop(bad_label_cols, axis=1)
+
+# Apply ordinal encoder to both X_train and X_Valid
+ordEncoder = OrdinalEncoder()
+label_X_train[good_label_cols] = ordEncoder.fit_transform(X_train[good_label_cols])
+label_X_valid [good_label_cols] = ordEncoder.transform(X_valid[good_label_cols])
+
+print("MAE from Approach 2 (Ordinal Encoding):") 
+print(score_dataset(label_X_train, label_X_valid, y_train, y_valid))
+
+#HOLD ON
+# Get number of unique entries in each column with categorical data
+object_nunique = list(map(lambda col: X_train[col].nunique(), object_cols))
+d = dict(zip(object_cols, object_nunique))
+
+# Print number of unique entries by column, in ascending order
+sorted(d.items(), key=lambda x: x[1])
+
+
+#Columns <10 Cardinality will be onehot encoded
+low_cardinality_cols = [col for col in object_cols if X_train[col].nunique() < 10]
+#high cardinality will be dropped from set
+high_cardinality_cols = list(set(object_cols)-set(low_cardinality_cols))
+
+# Apply one-hot encoder to each column with categorical data
+OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[low_cardinality_cols]))
+OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[low_cardinality_cols]))
+
+# One-hot encoding removed index; put it back
+OH_cols_train.index = X_train.index
+OH_cols_valid.index = X_valid.index
+
+# Remove categorical columns (will replace with one-hot encoding)
+num_X_train = X_train.drop(object_cols, axis=1)
+num_X_valid = X_valid.drop(object_cols, axis=1)
+
+# Add one-hot encoded columns to numerical features
+OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
+OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
+
+
